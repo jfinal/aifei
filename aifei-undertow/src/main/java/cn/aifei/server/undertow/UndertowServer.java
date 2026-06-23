9c7788141288c932b3bb2e390c3311394c59be5a
zhanbo <jfinal@126.com>
2026-06-23 16:44:27 +0800

    release: Aifei 1.0.3

--- aifei-undertow/src/main/java/cn/aifei/server/undertow/UndertowServer.java
+++ aifei-undertow/src/main/java/cn/aifei/server/undertow/UndertowServer.java
@@ -47,7 +47,7 @@ import static cn.aifei.server.undertow.UndertowConfig.*;
  */
 public class UndertowServer implements Server<HttpServerExchange, Void> {
 
-    static final String version = "1.0.2";
+    static final String version = "1.0.3";
     protected Builder builder;
     protected Undertow undertow;
     protected UndertowConfig config;
--- aifei/src/main/java/cn/aifei/core/Aifei.java
+++ aifei/src/main/java/cn/aifei/core/Aifei.java
@@ -34,7 +34,7 @@ import java.nio.file.Path;
  */
 public class Aifei {
 
-    static final String VERSION = "1.0.2";
+    static final String VERSION = "1.0.3";
 
     static Routes routes;
     static Plugins plugins;
